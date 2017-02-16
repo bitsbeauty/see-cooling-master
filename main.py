de8386fa8bdd3ab712f709d42f53d01ed0730042
DanielS <dschulze@bitsbeauty.de>
2017-02-17 00:47:54 +0100

    add mqtt-loop working inside of the main loop

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import time
 import paho.mqtt.client as mqtt
-import paho.mqtt.subscribe as subscribe
+# import paho.mqtt.subscribe as subscribe
 import json
 import database as db
 
@@ -10,26 +10,28 @@ import database as db
 MQTT_BROKER = "seebier.local"
 MQTT_PORT = 1883
 MQTT_KEEPALIVE_INTERVAL = 45
-MQTT_TOPIC = "/kuehlung/setTemp/kuehlung1"
-MQTT_MSG = "yes, Hello MQTT - Daniel, you did that!"
-
+MQTT_TOPIC = "/freezer/f0/isValues"
 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
+	print("Connected to MQTT Broker")
 	print("Connected with result code "+str(rc))
 
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
-	client.subscribe("$SYS/#")
+	# client.subscribe("$SYS/#")
+	mqttc.subscribe(MQTT_TOPIC,2)
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+	print("")
 	print(str(client)+" - "+str(userdata))
 	print(msg.topic+" -- "+str(msg.payload))
 
-
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 
 def on_message_kuehlung(client, userdata, message):
@@ -55,6 +57,10 @@ def on_message_kuehlung(client, userdata, message):
 
 
 f = [] # abstraction list of freezer
+
+# Initiate MQTT Client
+mqttc = mqtt.Client()
+
 
 # ---------------------------------------------------------------
 if __name__ == '__main__':
@@ -92,20 +98,32 @@ if __name__ == '__main__':
 
 	print "BEFORE"
 	#TODO: add QoS!!!
-	subscribe.callback(on_message_kuehlung, "/freezer/+/isValues", hostname=MQTT_BROKER)
+	# subscribe.callback(on_message_kuehlung, "/freezer/+/isValues", hostname=MQTT_BROKER)
+	mqttc.on_message = on_message_kuehlung
+	mqttc.on_connect = on_connect
+	# mqttc.on_publish = on_publish
+	# mqttc.on_subscribe = on_subscribe
 	print "AFTER"
+
+	# Connect
+	mqttc.connect(MQTT_BROKER)
+
 
 
 	try:
+		
 
 		while True:
+			mqttc.loop()
+
 			# 1) get the freezer temp and updtae in database
 			
 			f[0].updateValues()
 			if f[0].isStarted() is True:
 				print("Freezer %i is Started") %(f[0].id)
+				mqttc.publish("canyouhearme/01", "FREEZER")
 
-				print("Freezer Duration %i") %(f[0].getDurationStr())
+				# print("Freezer Duration %i") %(f[0].getDurationStr())
 
 
 			# check if temp is t
@@ -127,7 +145,7 @@ if __name__ == '__main__':
 		print "---CLEANUP:"
 		
 		db.db.close()
-		
+		mqttc.loop_stop()
 
 	# ---------------------------------------------------------------
 
